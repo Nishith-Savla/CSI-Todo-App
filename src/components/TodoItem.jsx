c2e21a7fa8d1c01f1aeb54cc0eb70df57f03e23c
Nishith.Savla <nishithsavla005@gmail.com>
2021-09-21 14:02:33 +0530

    :zap: Add debouncing for todo update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,27 @@
 import "./TodoItem.css";
 
+import { useState } from "react";
 import { Checkbox, IconButton, TextField } from "@material-ui/core";
 import { deleteDoc, doc, setDoc } from "firebase/firestore";
 
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import db from "../firebase";
 
+let timeout;
 const TodoItem = ({ id, completed, text }) => {
+	const [todo, setTodo] = useState(text);
 	const handleComplete = async () =>
 		await setDoc(doc(db, "todos", id), { completed: !completed, text });
 
-	const handleTextChange = async e =>
-		await setDoc(doc(db, "todos", id), { completed, text: e.target.value });
-
+	const handleTextChange = e => {
+		const { value } = e.target;
+		setTodo(value);
+		clearTimeout(timeout);
+		timeout = setTimeout(async () => {
+			if (text !== value)
+				await setDoc(doc(db, "todos", id), { completed, text: value });
+		}, 800);
+	};
 	const handleDelete = async e => await deleteDoc(doc(db, "todos", id));
 
 	return (
@@ -29,7 +38,7 @@ const TodoItem = ({ id, completed, text }) => {
 					name="todo"
 					multiline
 					style={{ width: "100%" }}
-					value={text}
+					value={todo}
 					onChange={handleTextChange}
 				/>
 			</div>
